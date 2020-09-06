1ddc5f85a1a0dece42f1b8cc1c811adcd63427ac
Niall O'Connor <niall.oconnor@holbmac0663.broadband>
2020-09-06 23:53:29 +0100

    feat: change to create react app

--- packages/client/src/components/Navigation/Navigation.js
+++ packages/client/src/components/Navigation/Navigation.js
@@ -9,37 +9,45 @@ import { theme } from "../../config";
 const AnalyticsMenuItem = withAnalytics(Item);
 
 export default () => {
-    const [{ activeItem }, setState] = useState({ activeItem: "home" });
+  const [{ activeItem }, setState] = useState({ activeItem: "home" });
 
-    const handleItemClick = (e, { name }) => setState({ activeItem: name });
+  const handleItemClick = (e, { name }) => setState({ activeItem: name });
 
-    return (
-        <Menu display="none" secondary size="huge" backgroundColor={theme.colors.main000} color={theme.colors.white000} anchorColor={theme.colors.white000} fontFamily={theme.defaultFont.fontFamily}>
-            <AnalyticsMenuItem
-                name="logo"
-                active={activeItem === "logo"}
-                onClick={handleItemClick}
-                action="navigation-logo-click"
-                href="/"
-            >
-                <Image
-                    maxWidth={remCalc(23)}
-                    alt="idea camels logo"
-                    src={theme.logos.main000}
-                />
-            </AnalyticsMenuItem>
-            <MenuMenu position="right">
-                {items.map((o) => (
-                    <AnalyticsMenuItem
-                        key={o}
-                        name={o}
-                        href="/coming-soon"
-                        active={activeItem === o}
-                        onClick={handleItemClick}
-                        action={`${o.replace(" ", "-")}-click`.toLowerCase()}
-                    />
-                ))}
-            </MenuMenu>
-        </Menu>
-    );
+  return (
+    <Menu
+      display="none"
+      secondary
+      size="huge"
+      backgroundColor={theme.colors.main000}
+      color={theme.colors.white000}
+      anchorColor={theme.colors.white000}
+      fontFamily={theme.defaultFont.fontFamily}
+    >
+      <AnalyticsMenuItem
+        name="logo"
+        active={activeItem === "logo"}
+        onClick={handleItemClick}
+        action="navigation-logo-click"
+        href="/"
+      >
+        <Image
+          maxWidth={remCalc(23)}
+          alt="idea camels logo"
+          src={theme.logos.main000}
+        />
+      </AnalyticsMenuItem>
+      <MenuMenu position="right">
+        {items.map((o) => (
+          <AnalyticsMenuItem
+            key={o}
+            name={o}
+            href="/coming-soon"
+            active={activeItem === o}
+            onClick={handleItemClick}
+            action={`${o.replace(" ", "-")}-click`.toLowerCase()}
+          />
+        ))}
+      </MenuMenu>
+    </Menu>
+  );
 };
