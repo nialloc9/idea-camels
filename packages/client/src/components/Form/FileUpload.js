174fe1015fb5fbc52f9c59d43a4e32a7e463c560
Niall O'Connor <nialloc9@gmail.com>
2022-01-18 02:51:03 +0000

    feat: added form for purchasing domain. Next up to fix template editing

--- packages/client/src/components/Form/FileUpload.js
+++ packages/client/src/components/Form/FileUpload.js
@@ -1,36 +1,66 @@
 import React from "react";
-import {useDropzone} from 'react-dropzone'
-import {Error} from "./Error";
-import {Block} from "../Styled/Block";
-import {Icon} from "../Styled/Icon";
-import {Label} from "../Styled/Label";
+import { useDropzone } from "react-dropzone";
+import { Error } from "./Error";
+import { Block } from "../Styled/Block";
+import { Icon } from "../Styled/Icon";
+import { Label } from "../Styled/Label";
 import { remCalc } from "../../../../utils/style";
 
-  /**
-   * 
-   * @param string label 
-   * @param string accept e.g "image/jpeg, image/png"
-   * @param string error 
-   * @param number maxFiles 
-   * @param func onSubmit 
-   * @param func onError 
-   * @param bool disabled 
-   * @param number minSize: in bytes 
-   * @param number maxSize: in bytes  
-   */
-export const FileUpload = ({ label, accept, error, maxFiles=1, onSubmit, onError, disabled=false, minSize, maxSize }) => {
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDropAccepted: onSubmit, onDropRejected: onError, accept, maxFiles, disabled, minSize, maxSize })
-    
-    return (
-      <div {...getRootProps()}>
-        
-        <input {...getInputProps()} />
-        
-        <Block display="flex" alignItems="center" textAlign="center" margin="33% 0 0 0" color="#686868" hoverOpacity="0.5" opacity={isDragActive ? "0.5" : "1"} borderStyle="dashed" cursor="pointer" border={`${remCalc(1)} dashed #686868`} borderRadius={remCalc(10)} padding={remCalc(5)}>
-            <Icon name="plus circle" size="big" color="#686868" />
-            <Label cursor="pointer" label={label} />
-        </Block>
-        <Error error={error} />
-      </div>
-    )
-  }
+/**
+ *
+ * @param string label
+ * @param string accept e.g "image/jpeg, image/png"
+ * @param string error
+ * @param number maxFiles
+ * @param func onSubmit
+ * @param func onError
+ * @param bool disabled
+ * @param number minSize: in bytes
+ * @param number maxSize: in bytes
+ */
+export const FileUpload = ({
+  label,
+  accept,
+  error,
+  maxFiles = 1,
+  onSubmit,
+  onError,
+  disabled = false,
+  minSize,
+  maxSize,
+}) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDropAccepted: onSubmit,
+    onDropRejected: onError,
+    accept,
+    maxFiles,
+    disabled,
+    minSize,
+    maxSize,
+  });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+
+      <Block
+        display="flex"
+        alignItems="center"
+        textAlign="center"
+        margin="33% 0 0 0"
+        color="#686868"
+        hoverOpacity="0.5"
+        opacity={isDragActive ? "0.5" : "1"}
+        borderStyle="dashed"
+        cursor="pointer"
+        border={`${remCalc(1)} dashed #686868`}
+        borderRadius={remCalc(10)}
+        padding={remCalc(5)}
+      >
+        <Icon name="plus circle" size="big" color="#686868" />
+        <Label cursor="pointer" label={label} />
+      </Block>
+      <Error error={error} />
+    </div>
+  );
+};
