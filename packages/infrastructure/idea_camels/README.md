2f40b59f19ca20c57455a93018a7b91e2e977e8f
Niall O'Connor <nialloc9@gmail.com>
2022-02-08 20:37:27 +0000

    feat: added select template. Next up test deploying site through idea camels.

--- packages/infrastructure/idea_camels/README.md
+++ packages/infrastructure/idea_camels/README.md
@@ -8,3 +8,5 @@ Need to change named servers in registered domain to match new hosted zone
 - Cors not working and has to be manually triggered in the console
 
 PEM file created during deployment via cicd will be available for 5 days and then removed.
+
+If getting internal server error message go to api gateway and go to method (POST) and the 'Integration Request'. Click lambda function and reselect same lambda. Then click 'Use Lambda Proxy integration'. This will error but thats okay. Now it should have correct permissions for invoking the lambda. Then go to api gateway and hit 'Deploy API'
--- packages/infrastructure/idea_camels/vpc.tf
+++ packages/infrastructure/idea_camels/vpc.tf
@@ -38,7 +38,27 @@ resource "aws_eip" "ideacamels_main" {
   depends_on = [aws_internet_gateway.ideacamels_main]
 }
 
+resource "aws_eip" "ideacamels_main_2" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ideacamels_main]
+}
+
+resource "aws_eip" "ideacamels_main_3" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ideacamels_main]
+}
+
 resource "aws_nat_gateway" "ideacamels_main" {
   allocation_id = aws_eip.ideacamels_main.id
   subnet_id     = aws_subnet.idea_camels_main_public[0].id
+}
+
+resource "aws_nat_gateway" "ideacamels_main_1" {
+  allocation_id = aws_eip.ideacamels_main.id
+  subnet_id     = aws_subnet.idea_camels_main_public[1].id
+}
+
+resource "aws_nat_gateway" "ideacamels_main_2" {
+  allocation_id = aws_eip.ideacamels_main.id
+  subnet_id     = aws_subnet.idea_camels_main_public[2].id
 }
