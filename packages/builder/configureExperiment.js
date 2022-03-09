b57dfd886c743d3ccde686bfa7785979c07e108d
Niall O'Connor <nialloc9@gmail.com>
2022-03-09 19:35:51 +0000

    feat: added create campaign. Next up payments..

--- packages/builder/configureExperiment.js
+++ packages/builder/configureExperiment.js
@@ -20,7 +20,7 @@ const main = async () => {
     // TODO: run cron to update database to expired for domains going to expire tomorrow
     // TODO: run cron to send email for domains going to expire in 1 month and in 1 week
 
-    logger.info(`========= EXPERIMENT ${experimentRef} CREATED =========`);
+    logger.info(`=========  EXPERIMENT ${experimentRef} CREATED  =========`);
     process.exit(0);
   } catch (error) {
     logger.error(error);
