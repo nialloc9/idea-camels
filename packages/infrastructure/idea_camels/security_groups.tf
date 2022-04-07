43461379e7387790b9e7ba4ceb1c0c9509176c6c
Niall O'Connor <nialloc9@gmail.com>
2022-04-07 18:16:50 +0100

    feat: fix axios post not working with lambda api. Next up e2e test..

--- packages/infrastructure/idea_camels/security_groups.tf
+++ packages/infrastructure/idea_camels/security_groups.tf
@@ -54,6 +54,24 @@ module "db_security_group" {
       "protocol"          = "all",
       "cidr_blocks"       = null,
       "security_group_id" = module.bastion_security_group.id
+    },
+    {
+      "name"              = "ingress",
+      "type"              = "ingress",
+      "from_port"         = 0,
+      "to_port"           = 0,
+      "protocol"          = "all",
+      "cidr_blocks"       = null,
+      "security_group_id" = module.builder_security_group.id
+    },
+    {
+      "name"              = "egress",
+      "type"              = "egress",
+      "from_port"         = 0,
+      "to_port"           = 0,
+      "protocol"          = "all",
+      "cidr_blocks"       = null,
+      "security_group_id" = module.builder_security_group.id
     }
   ]
 }
