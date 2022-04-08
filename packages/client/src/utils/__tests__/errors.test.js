63be948af41dbf5fa3706677345b1ff6f648cfaa
Niall O'Connor <nialloc9@gmail.com>
2022-04-08 10:27:25 +0100

    feat: fix axios post not working with lambda api. Next up e2e test..

--- packages/client/src/utils/__tests__/errors.test.js
+++ packages/client/src/utils/__tests__/errors.test.js
@@ -2,11 +2,11 @@ import { getError } from "../errors";
 
 describe("error utils test suite", () => {
   it("returns correct error", () => {
-    expect(getError({ coe: 1000 })).toEqual("Email already in use.");
-    expect(getError({ coe: 1002 })).toEqual("Account not found.");
-    expect(getError({ coe: 1005 })).toEqual("Domain not available.");
-    expect(getError({ coe: 0101 })).toEqual(
-      "An error has occured. Please reload your browser. Code: 0101."
+    expect(getError({ code: 1000 })).toEqual("Email already in use.");
+    expect(getError({ code: 1002 })).toEqual("Account not found.");
+    expect(getError({ code: 1005 })).toEqual("Domain not available.");
+    expect(getError({ code: 743759435 })).toEqual(
+      "An error has occured. Please reload your browser. Code: 743759435."
     );
     expect(getError({})).toEqual(
       "An error has occured. Please reload your browser. Code: 500."
