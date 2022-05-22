e01a3fad1285b0ee8bfc79b5a473e61ff13fe91f
Niall <nialloc9@gmail.com>
2022-05-22 21:18:45 +0100

    fixes: add api dependency on aws_api_gateway_method

--- packages/infrastructure/idea_camels/api.tf
+++ packages/infrastructure/idea_camels/api.tf
@@ -132,7 +132,8 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 resource "aws_api_gateway_deployment" "apideploy" {
   depends_on = [
-    aws_api_gateway_integration.lambda_root
+    aws_api_gateway_integration.lambda_root,
+    aws_api_gateway_method.proxy_root,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
