fde69d4034d8544f3bb457a6533060e336a26fff
Niall <nialloc9@gmail.com>
2022-05-22 23:57:24 +0100

    fixes: add api dependency on aws_api_gateway_method

--- packages/builder/templates/1/components/Head/index.js
+++ packages/builder/templates/1/components/Head/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NextHead from "next/head";
 import { config, content } from "config";
-import { concatArrayToString } from "utils/utils";
+import { concatArrayToString } from "../../utils/utils";
 
 export default function Head({}) {
   const metaData = [
--- packages/builder/templates/1/components/Login/ForgottonPasswordForm.js
+++ packages/builder/templates/1/components/Login/ForgottonPasswordForm.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { remCalc } from "../../utils/utils";
 import { FormInput } from "../Form/Input";
 import Form from "../Form/Form";
