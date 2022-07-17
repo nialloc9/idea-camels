44a5b7a2faa5bd39076b54ff90fab68da969aee7
Niall <nialloc9@gmail.com>
2022-07-17 12:22:53 +0100

    feat: run cron every sun at 10:00

--- packages/infrastructure/modules/experiment/certificate.tf
+++ packages/infrastructure/modules/experiment/certificate.tf
@@ -10,7 +10,7 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_acm_certificate_validation" "cert" {
   provider                = "aws.cloudfront"
   count = var.certificate_arn != "" ? 1 : 0
-  certificate_arn         = aws_acm_certificate[0].cert.arn
+  certificate_arn         = aws_acm_certificate.cert[0].arn
   validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
 
   depends_on = [aws_acm_certificate.cert]
