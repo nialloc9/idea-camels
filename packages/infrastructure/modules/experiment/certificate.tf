8a54ea3e057eafe9534ec6e966c2bfb831d4cab7
Niall <nialloc9@gmail.com>
2022-07-17 12:43:51 +0100

    feat: run cron every sun at 10:00

--- packages/infrastructure/modules/experiment/certificate.tf
+++ packages/infrastructure/modules/experiment/certificate.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "cert" {
   provider                  = "aws.cloudfront"
-  count = var.certificate_arn != "" ? 1 : 0
+  count = var.certificate_arn == "" ? 1 : 0
   domain_name               = var.fqdn
   validation_method         = var.cert_validation_method
   subject_alternative_names = ["www.${var.fqdn}"]
@@ -9,7 +9,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_acm_certificate_validation" "cert" {
   provider                = "aws.cloudfront"
-  count = var.certificate_arn != "" ? 1 : 0
+  count = var.certificate_arn == "" ? 1 : 0
   certificate_arn         = aws_acm_certificate.cert[0].arn
   validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
 
--- packages/infrastructure/modules/experiment/route_53.tf
+++ packages/infrastructure/modules/experiment/route_53.tf
@@ -30,7 +30,7 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "cert_validation" {
   provider = "aws.cloudfront"
-  count   = var.create_cert_validations ? length(aws_acm_certificate.cert[0].subject_alternative_names) : 0
+  count   = var.create_cert_validations && var.certificate_arn=="" ? length(aws_acm_certificate.cert[0].subject_alternative_names) : 0
   zone_id = aws_route53_zone.main.zone_id
   name    = element(aws_acm_certificate.cert[0].domain_validation_options.*.resource_record_name, count.index)
   type    = element(aws_acm_certificate.cert[0].domain_validation_options.*.resource_record_type, count.index)
