cb86b49cf335c9950033827b2e828d871aebf0db
Niall <nialloc9@gmail.com>
2022-07-17 12:16:26 +0100

    feat: run cron every sun at 10:00

--- packages/infrastructure/modules/experiment/route_53.tf
+++ packages/infrastructure/modules/experiment/route_53.tf
@@ -30,11 +30,11 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "cert_validation" {
   provider = "aws.cloudfront"
-  count   = var.create_cert_validations ? length(aws_acm_certificate.cert.subject_alternative_names) : 0
+  count   = var.create_cert_validations ? length(aws_acm_certificate.cert[0].subject_alternative_names) : 0
   zone_id = aws_route53_zone.main.zone_id
-  name    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
-  type    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
-  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
+  name    = element(aws_acm_certificate.cert[0].domain_validation_options.*.resource_record_name, count.index)
+  type    = element(aws_acm_certificate.cert[0].domain_validation_options.*.resource_record_type, count.index)
+  records = [element(aws_acm_certificate.cert[0].domain_validation_options.*.resource_record_value, count.index)]
   ttl     = 60
 
   depends_on = [aws_acm_certificate.cert, null_resource.aws_domain_com_nameservers]
