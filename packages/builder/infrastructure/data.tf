0406d8d9af31be5206798785a42c329c1231bb18
Niall <nialloc9@gmail.com>
2022-07-17 23:51:11 +0100

    fix: incorrect path for builder

--- packages/builder/infrastructure/data.tf
+++ packages/builder/infrastructure/data.tf
@@ -1,4 +1,11 @@
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+}
+
 data "aws_acm_certificate" "ideacamels" {
   domain   = "ideacamels.com"
   statuses = ["ISSUED"]
+
+  provider = aws.virginia
 }
