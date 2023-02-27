dd66b9b5d0ecb33c000965ec5b83e302ec557443
Niall <nialloc9@gmail.com>
2023-02-27 23:50:38 +0000

    fix: move timeout from header

--- packages/api/main.js
+++ packages/api/main.js
@@ -11,7 +11,8 @@ const responseHandler = {
         "Access-Control-Allow-Origin": "*", // Required for CORS support to work
         "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
         "Access-Control-Allow-Methods": "POST,OPTIONS,GET",
-        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization",
       },
       body: JSON.stringify(body),
     };
@@ -27,7 +28,8 @@ const responseHandler = {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*", // Required for CORS support to work
         "Access-Control-Allow-Methods": "POST,OPTIONS,GET",
-        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization",
       },
       body: JSON.stringify({
         statusCode: 500,
